e8c73592eac2bae021c4ee5319d07f78c3993625
Jill Henderson <jillian.bethany.henderson@gmail.com>
2023-09-13 17:40:26 +0100

    index JS done

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,42 @@
 import { createRoot } from 'react-dom/client'
 import './styles/main.scss'
 import App from './App'
+import express from 'express'
+import mongoose from 'mongoose'
+import router from './config/routes.js'
+import 'dotenv/config'
+
+const app = express()
+app.use(express.json())
+app.use(router)
+
+
+
+app.use((req, res, next) => {
+  console.log(`🚨 Request received: ${req.method} ${req.url}`)
+  next()
+})
+
+
+
+// ?  Route Not Found
+app.use((req, res) => {
+  return res.status(404).json({ message: 'Route not found' })
+})
+
+const startServer = async () => {
+  try {
+
+    await mongoose.connect(process.env.CONNECTION_STRING)
+    console.log('🌱 Database connection established')
+
+
+    app.listen(process.env.PORT, () => console.log(`🚀 Server listening on port ${process.env.PORT}`))
+  } catch (error) {
+    console.log('🆘 Something went wrong when starting the server')
+    console.log(error)
+  }
+}
+startServer()
 
 createRoot(document.getElementById('root')).render(<App />)
