b8f6e610f1acfdcdffb16dfcb32f725261a8ad85
Jill Henderson <jillian.bethany.henderson@gmail.com>
2023-09-13 19:36:44 +0100

    dummy data and blogs added

---
+++ db/data/blogs.js
@@ -0,0 +1,133 @@
+export default [
+
+  {
+    image: 'healthy_breakfast_ideas.png',
+    title: 'Revolutionize Your Breakfast',
+    author: 'Jane Doe',
+    dateCreated: '2023-09-11',
+    content: `Breakfast is often called the most important meal of the day, and for good reason. Not only does it provide the essential nutrients and energy needed to kickstart your morning, but a healthy breakfast also sets the stage for the choices you make throughout the rest of the day. However, we often find ourselves stuck in a breakfast rut, eating the same cereal or toast day in and day out. To inspire you to revolutionize your mornings, here are some healthy breakfast ideas that are both delicious and nutritious.
+  
+    ### Overnight Oats
+    Overnight oats are the perfect solution for those who always seem to be in a rush in the morning. All you need to do is combine rolled oats, your choice of milk (almond, soy, cow's milk), a touch of sweetener, and your favorite fruits and nuts in a jar. Let the mixture sit in the refrigerator overnight, and you'll wake up to a delicious, ready-to-eat breakfast.
+  
+    ### Avocado Toast with a Twist
+    Avocado toast has been a breakfast staple for years, but have you ever tried elevating it with some unconventional toppings? A sprinkle of pomegranate seeds can add a surprising burst of flavor, while a drizzle of tahini will give your toast a creamy, nutty kick.
+  
+    ### Smoothie Bowls
+    Instead of sipping on a smoothie, why not turn it into a meal? Create a thicker version of your favorite smoothie and pour it into a bowl. Then, top it off with granola, fresh fruits, and even a handful of dark chocolate chips for a breakfast that feels indulgent but is packed with nutrients.
+  
+    ### Chia Pudding
+    Chia seeds are rich in Omega-3 fatty acids and fiber, making them a great ingredient to include in your breakfast. To make chia pudding, simply mix chia seeds with your choice of milk and let the mixture sit in the refrigerator until it thickens. Add fruits, nuts, and a drizzle of honey for extra flavor.
+  
+    ### Veggie Omelet
+    An omelet is an excellent way to incorporate vegetables into your breakfast. Whisk together some eggs, pour them into a hot pan, and before the eggs fully set, add your choice of veggies. Spinach, bell peppers, and mushrooms are all great options.
+  
+    ### Nut Butter and Banana Sandwich
+    For those who prefer a breakfast they can eat on the go, a nut butter and banana sandwich is an ideal option. Spread almond or peanut butter between two slices of whole-grain bread, add banana slices, and you have a sandwich that is both satisfying and healthy.
+  
+    The key to a revolutionary breakfast is not just what you eat but also how you prepare it. Incorporate fresh ingredients, experiment with new recipes, and take the time to savor each bite. Not only will these healthy breakfast ideas help improve your physical well-being, but they'll also give you the mental boost needed to tackle whatever challenges the day may bring.`
+  },
+  {
+    image: 'turmeric_health_benefits.png',
+    title: 'Unlocking the Health Benefits of Turmeric',
+    author: 'John Smith',
+    dateCreated: '2023-09-11',
+    content: `Turmeric, the vibrant yellow spice commonly used in Asian cuisine, has been a staple in traditional medicine for thousands of years. More recently, scientific studies have started to confirm its myriad of health benefits, making it more than just a flavorful addition to your curry. In this blog post, we'll delve into some of the remarkable health benefits of turmeric and how you can incorporate it into your daily diet.
+  
+    ### Anti-Inflammatory Properties
+    One of the most well-documented benefits of turmeric is its powerful anti-inflammatory effects. Curcumin, the active ingredient in turmeric, has been shown to be as effective as some anti-inflammatory drugs, without the side effects.
+  
+    ### Antioxidant Boost
+    Curcumin is also a potent antioxidant that can neutralize free radicals while boosting the activity of your body's own antioxidant enzymes. This one-two punch makes it incredibly effective in combating oxidative stress, a key factor in aging and many diseases.
+  
+    ### Brain Functionality
+    Curcumin has been linked to improved brain function and a lower risk of brain-related diseases. It boosts levels of Brain-Derived Neurotrophic Factor (BDNF), a growth hormone that functions in the brain, which could potentially delay or even reverse brain diseases or age-related decreases in brain function.
+  
+    ### Natural Antidepressant
+    Some promising research suggests that curcumin could be used in treating depression. It boosts serotonin and dopamine levels, which can lead to improved mood. While more research is needed, the early results are encouraging.
+  
+    ### Lowering Heart Disease Risk
+    Curcumin improves the function of the endothelium, the lining of the blood vessels, which plays a significant role in heart health. It has been shown to reduce inflammation and oxidation, leading to a reduced risk of heart disease.
+  
+    ### Other Benefits
+    Other health benefits attributed to turmeric include potential cancer prevention, improved arthritis symptoms, and diabetes management. However, it's worth noting that many of these studies are preliminary and further research is needed to confirm these effects.
+  
+    ### How to Incorporate Turmeric into Your Diet
+    Turmeric can be easily incorporated into your diet. It pairs well with a variety of foods, from scrambled eggs and soups to smoothies and rice dishes. One popular way to consume it is by making "Golden Milk," a warm beverage made with milk, turmeric, and other spices like cinnamon and ginger.
+  
+    In conclusion, the health benefits of turmeric make it a worthy addition to anyone's diet. Whether you're trying to reduce inflammation, boost your antioxidants, or simply add some zest to your meals, turmeric has something to offer. So the next time you're cooking, consider reaching for that jar of turmeric to not just spice up your dish, but also your life.`
+  },
+  {
+    image: 'vegan_protein_sources.png',
+    title: 'Plant-Powered Protein: A Vegan’s Guide',
+    author: 'Emily Johnson',
+    dateCreated: '2023-09-11',
+    content: `One of the most common questions vegans get asked is, "Where do you get your protein?" The misconception that a plant-based diet lacks sufficient protein has been debunked time and time again, but the question still persists. In this blog post, we’ll explore various plant-based protein sources and how to incorporate them into your daily meals.
+  
+    ### Legumes: The Protein Powerhouse
+    Lentils, chickpeas, and other legumes are some of the most protein-dense plant foods available. A cup of cooked lentils can offer around 18 grams of protein. You can easily make hearty salads, stews, and curries packed with these protein powerhouses.
+  
+    ### Tofu and Tempeh: Versatile Staples
+    Tofu and tempeh are made from soybeans and are highly versatile in the kitchen. From tofu scramble for breakfast to tempeh stir-fries for dinner, these foods offer around 15-20 grams of protein per serving.
+  
+    ### Whole Grains: More Than Just Carbs
+    Quinoa, farro, and barley are not just carbohydrate sources; they also provide a decent amount of protein. A cup of cooked quinoa, for instance, provides around 8 grams of protein.
+  
+    ### Nuts and Seeds: Snack With Purpose
+    Almonds, chia seeds, and pumpkin seeds are great for snacking or adding a crunch to your salads. They contain around 5-7 grams of protein per ounce. Remember, they are also high in calories and fats, so moderation is key.
+  
+    ### Plant-Based Dairy: Not Just for Smoothies
+    Plant-based milk like almond milk, soy milk, and oat milk often come fortified with protein, making them a good addition to your meals. Use them in cooking, baking, or smoothies for added nutritional value.
+  
+    ### Protein-Rich Veggies: The Overlooked Heroes
+    Believe it or not, some vegetables like spinach, broccoli, and Brussels sprouts contain decent amounts of protein. They may not provide as much protein per serving as other sources, but they are an integral part of a balanced vegan diet.
+  
+    ### Supplements: A Helping Hand
+    Vegan protein powders made from hemp, rice, or pea protein can provide an extra boost, especially for athletes or highly active individuals. However, whole foods should always be the primary source of nutrition.
+  
+    ### Tips for Meal Planning
+    The key to a protein-rich vegan diet is variety. Make sure to incorporate different protein sources throughout your day. You can also combine foods like legumes and grains to create a complete protein, which provides all nine essential amino acids.
+  
+    Being vegan doesn’t mean compromising on protein or relying on supplements. Mother Nature offers a cornucopia of protein-rich foods that are plant-based, proving that you can be vegan and still meet all your nutritional needs. So the next time someone asks, "Where do you get your protein?", you’ll have plenty to say.`
+  },
+  {
+    image: 'saving_on_groceries.png',
+    title: 'Smart Shopping: Tips to Save Money on Groceries',
+    author: 'Alex Williams',
+    dateCreated: '2023-09-11',
+    content: `Food shopping is a significant part of any household's budget, but it's also one area where you can make smart choices to save money without sacrificing quality. If you’ve ever felt the pinch when looking at your grocery bill, this blog is for you. Below are some tips and tricks to help you save money the next time you hit the supermarket.
+  
+    ### Make a List and Stick to It
+    One of the best ways to avoid impulse buying is to make a shopping list before you go to the store. This helps you buy only what you need and minimizes the chances of filling your cart with items that catch your eye but aren’t really necessary.
+  
+    ### Shop with a Full Stomach
+    This might sound like a weird tip, but shopping when you're hungry tends to result in unnecessary purchases. A full stomach will make it easier to stick to your list.
+  
+    ### Use Coupons and Discounts Wisely
+    Coupons and discounts can be your best friends, but only if you use them for products you were planning to buy anyway. Don’t be lured into buying something just because there’s a coupon for it.
+  
+    ### Buy Generic Brands
+    Store brands or generic brands often offer the same quality as name brands but at a fraction of the cost. Don’t hesitate to opt for these to save some bucks.
+  
+    ### Buy in Bulk, but Be Smart About It
+    While buying in bulk is generally cheaper, make sure that you’ll use all of it before the item goes bad. This is particularly important for perishables like fruits and vegetables.
+  
+    ### Seasonal and Local is the Way to Go
+    Fruits and vegetables that are in season and locally sourced are usually cheaper and fresher. They also have the added benefit of being more environmentally friendly.
+  
+    ### Use Apps and Technology
+    Many supermarkets have apps that offer exclusive promotions and digital coupons. Some apps even keep track of what you buy frequently and offer discounts on those items.
+  
+    ### Plan Your Meals
+    Meal planning helps you make a precise shopping list and minimizes waste. You'll know exactly what you need for the week and can better take advantage of sales and bulk deals.
+  
+    ### Keep an Eye on the Price Per Unit
+    Don’t just look at the total price; look at the price per unit or per ounce to truly understand if you’re getting a good deal. This is especially helpful when comparing different brands or sizes.
+  
+    ### Don’t Be Afraid to Freeze
+    If you find a good deal on something that can be frozen, like meat or bread, don’t be afraid to stock up and freeze the extras. Just make sure to use them before they get freezer burn.
+  
+    By applying these tips, you'll find that your food bills start to decrease, leaving you with extra money to use elsewhere. Saving on groceries doesn't mean you have to compromise on quality or variety; it simply means you need to shop smarter. Happy shopping!`
+  },
+  
+]
---
+++ db/data/users.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    username: 'admin',
+    email: 'admin@gmail.com',
+    password: 'adminpass',
+    passwordConfirmation: 'adminpass',
+  },
+  {
+    username: 'kisalie',
+    email: 'kisalie@gmail.com',
+    password: 'kisaliepass',
+    passwordConfirmation: 'kisaliepass',
+  },
+  {
+    username: 'luke',
+    email: 'luke@gmail.com',
+    password: 'lukepass',
+    passwordConfirmation: 'lukepass',
+  },
+  {
+    username: 'Jilly',
+    email: 'jilly@gmail.com',
+    password: 'jillypass',
+    passwordConfirmation: 'jillypass',
+  },
+  {
+    username: 'Tommy',
+    email: 'tommy@gmail.com',
+    password: 'tommypass',
+    passwordConfirmation: 'tommypass',
+  },
+  {
+    username: 'Sally',
+    email: 'sally@gmail.com',
+    password: 'sallypass',
+    passwordConfirmation: 'sallypass',
+  },
+  {
+    username: 'Frank',
+    email: 'frank@gmail.com',
+    password: 'frankpass',
+    passwordConfirmation: 'frankpass',
+  },
+  {
+    username: 'Linda',
+    email: 'linda@gmail.com',
+    password: 'lindapass',
+    passwordConfirmation: 'lindapass',
+  },
+  {
+    username: 'Steve',
+    email: 'steve@gmail.com',
+    password: 'stevepass',
+    passwordConfirmation: 'stevepass',
+  }  
+]
