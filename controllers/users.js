cc2b61c66e099d8ff9c85bddb8bf7a88bcfff360
kisalier <kisalie@hotmail.com>
2023-09-13 21:41:21 +0100

    added users coontrollers

---
+++ controllers/users.js
@@ -0,0 +1,34 @@
+import User from '../models/users.js'
+import jwt from 'jsonwebtoken'
+
+// Register route
+export const registerUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    return res.status(201).json({ message: `Welcome ${user.username}`})
+  } catch (error) {
+    console.log(error)
+    return res.status(422).json(error)
+  }
+}
+
+// Login route
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const userLogin = await User.findOne({ email: email })
+
+    if(!userLogin) throw new Error('User not found')
+
+    if(!userLogin.validatePassword(password)){
+      throw new Error('Password invalid')
+    }
+    
+    const token = jwt.sign({ sub: userLogin._id }, process.env.SECRET, { expiresIn: '5d'})
+    return res.json({ message: `Welcome back, ${userLogin.username}!`, token: token })
+  } catch (error) {
+    console.log(error)
+    return res.status(401).json({ error: 'Unauthorized'})
+  }
+}
