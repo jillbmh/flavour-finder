7945885af7f64b47259b7f163cd02396a4ab37ea
kisalier <kisalie@hotmail.com>
2023-09-15 12:35:38 +0100

    added changes

--- db/data/recipes.js
+++ db/data/recipes.js
@@ -2,7 +2,7 @@ export default [
   {
     title: 'Sushi',
     image: 'sushi.png',
-    cuisine: 'Japan',
+    cuisine: 'Japanese',
     ingredients: [{ ingredient: 'Rice', amount: '300 grams' }, { ingredient: 'fish', amount: '225 grams' }, { ingredient: 'seaweed', amount: '25 grams' }, { ingredient: 'vegetables', amount: '500 grams' }],
     cookingTime: { hours: 0, minutes: 15 },
     type: 'main',
