104d0d5d0a96fd4b895b1136468b8bf8ee6989a5
Luke Horton <developedbyluke@gmail.com>
2023-09-16 12:19:41 +0100

    Added new logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,7 @@ import AccountIcon from '../images/account-icon.png'
 import SearchIcon from '../images/search-icon.png'
 import MenuIcon from '../images/menu-icon.png'
 import LogoText from '../images/logo-text.png'
+import NewLogo from '../images/new-logo.png'
 
 export default function Header() {
   return (
@@ -15,15 +16,20 @@ export default function Header() {
 
       {/* Logo stuff */}
       <Link to='/'>
-        <div className='logo-container'>
+        {/* <div className='logo-container'>
           <img src={Logo}></img>
           <img src={LogoText}></img>
-        </div>
+        </div> */}
+        <img src={NewLogo}></img>
       </Link>
 
       {/* Icons */}
-      <div className='header-buttons-container'>
-        <img src={SearchIcon}></img>
+      <div className='header-buttons'>
+        <div className='search-bar'>
+          <input type='text' />
+          <img src={SearchIcon}></img>
+        </div>
+        <img src={SearchIcon} className='header-buttons-search'></img>
         <img src={AccountIcon}></img>
         <img src={MenuIcon} width='18px' className='menu-icon'></img>
       </div>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,5 +2,5 @@ import { useEffect } from 'react'
 import axios from 'axios'
 
 export default function Home() {  
-  return <h1>Home</h1>
+  return
 }
