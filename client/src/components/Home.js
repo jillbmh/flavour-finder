538115434ca6f2234aabea664a8a081e520ce3ed
Luke Horton <developedbyluke@gmail.com>
2023-09-16 12:39:17 +0100

    Edited home page animation section and fixed single blog CSS that was causing all images across the website to have a border

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,21 +2,14 @@ import React from 'react'
 import AnimationImage from '../images/animation-image.jpeg'
 
 export default function Home() {
-
   return (
     <>
-      <h1>Home</h1>
       <div className='animation-container'>
-        <img src={AnimationImage} alt="Animation" />
-        <div 
-          id="animated-words" 
-          className="animated-words"
-        >
+        <img src={AnimationImage} alt='Animation' />
+        <div id='animated-words' className='animated-words'>
           delicious
         </div>
-        <h1 className="hero-heading">
-          Flavour Finder
-        </h1>
+        <h1 className='hero-heading'>Recipe Title</h1>
       </div>
     </>
   )
