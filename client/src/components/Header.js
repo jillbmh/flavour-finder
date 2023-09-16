d9707f25d2bd5c3ebcd67dfba6ec402f56375ea3
Luke Horton <developedbyluke@gmail.com>
2023-09-17 00:30:58 +0100

    Added menu icon animation for when the menu opens/closes and also started the menu itself which slides in from the left.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,39 +1,68 @@
 import { Link } from 'react-router-dom'
-import Logo from '../images/logo.png'
 import AccountIcon from '../images/account-icon.png'
 import SearchIcon from '../images/search-icon.png'
 import MenuIcon from '../images/menu-icon.png'
-import LogoText from '../images/logo-text.png'
+import CloseIcon from '../images/close-icon.png'
 import NewLogo from '../images/new-logo.png'
+import { useState, useEffect } from 'react'
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [animation, setAnimation] = useState('none')
+  const [openSideMenu, setOpenSideMenu] = useState(false)
+
+  useEffect(() => {
+    if (animation === 'shrinking') {
+      setTimeout(() => {
+        setIsMenuOpen(!isMenuOpen)
+        setAnimation('growing')
+      }, 200) // 300ms matches the animation duration. Adjust if needed.
+    } else if (animation === 'growing') {
+      setTimeout(() => {
+        setAnimation('none')
+      }, 200) // 300ms matches the animation duration. Adjust if needed.
+    }
+  }, [animation])
+
+  const handleClick = () => {
+    setAnimation('shrinking')
+    setOpenSideMenu(!openSideMenu)
+  }
+
   return (
     <header>
-      {/* Burger menu for mobile */}
-      <img src={MenuIcon} width='18px' className='menu-icon'></img>
-
-      {/* Tablet Up Menu */}
-      <div className='tablet-up-menu-items'>
-        <Link to='/recipes'>Recipes</Link>
+      {/* MENU ICON */}
+      <div className='menu-icon-container'>
+        <img
+          src={isMenuOpen ? CloseIcon : MenuIcon}
+          width={isMenuOpen ? '14px' : '18px'}
+          className={`menu-icon ${
+            animation === 'shrinking' ? 'shrink-icon' : animation === 'growing' ? 'grow-icon' : ''
+          }`}
+          onClick={handleClick}
+          alt='menu-icon'
+        />
       </div>
 
-      {/* Logo stuff */}
+      {/* LOGO */}
       <Link to='/'>
-        {/* <div className='logo-container'>
-          <img src={Logo}></img>
-          <img src={LogoText}></img>
-        </div> */}
-        <img src={NewLogo}></img>
+        <img src={NewLogo} className='logo'></img>
       </Link>
 
-      {/* Icons */}
+      {/* BUTTONS */}
       <div className='header-buttons'>
         <div className='search-bar'>
-          <input type='text' />
+          <input type='text' placeholder='Search...' />
           <img src={SearchIcon}></img>
         </div>
         <img src={SearchIcon} className='header-buttons-search'></img>
         <img src={AccountIcon}></img>
+      </div>
+
+      {/* MENU */}
+      <div className={`side-menu ${openSideMenu ? 'open' : ''}`}>
+        <Link to='/recipes'>Recipes</Link>
+        <Link to='/blogs'>Blogs</Link>
       </div>
     </header>
   )
