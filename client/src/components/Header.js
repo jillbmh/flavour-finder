24681a0a1a24969004e6ddfa15b6dcba56150621
Luke Horton <developedbyluke@gmail.com>
2023-09-17 02:13:31 +0100

    Separated header and menu and put them under a container so that the menu can show below the header in their own flow rather than overlapping each other or having to add any margin

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,7 @@ import SearchIcon from '../images/search-icon.png'
 import MenuIcon from '../images/menu-icon.png'
 import CloseIcon from '../images/close-icon.png'
 import NewLogo from '../images/new-logo.png'
+import ArrowIcon from '../images/arrow-icon.png'
 import { useState, useEffect } from 'react'
 
 export default function Header() {
@@ -16,11 +17,11 @@ export default function Header() {
       setTimeout(() => {
         setIsMenuOpen(!isMenuOpen)
         setAnimation('growing')
-      }, 200) // 300ms matches the animation duration. Adjust if needed.
+      }, 250) // 300ms matches the animation duration. Adjust if needed.
     } else if (animation === 'growing') {
       setTimeout(() => {
         setAnimation('none')
-      }, 200) // 300ms matches the animation duration. Adjust if needed.
+      }, 250) // 300ms matches the animation duration. Adjust if needed.
     }
   }, [animation])
 
@@ -30,40 +31,49 @@ export default function Header() {
   }
 
   return (
-    <header>
-      {/* MENU ICON */}
-      <div className='menu-icon-container'>
-        <img
-          src={isMenuOpen ? CloseIcon : MenuIcon}
-          width={isMenuOpen ? '14px' : '18px'}
-          className={`menu-icon ${
-            animation === 'shrinking' ? 'shrink-icon' : animation === 'growing' ? 'grow-icon' : ''
-          }`}
-          onClick={handleClick}
-          alt='menu-icon'
-        />
-      </div>
-
-      {/* LOGO */}
-      <Link to='/'>
-        <img src={NewLogo} className='logo'></img>
-      </Link>
-
-      {/* BUTTONS */}
-      <div className='header-buttons'>
-        <div className='search-bar'>
-          <input type='text' placeholder='Search...' />
-          <img src={SearchIcon}></img>
+    <div className='header-menu-container'>
+      <header>
+        {/* MENU ICON */}
+        <div className='menu-icon-container'>
+          <img
+            src={isMenuOpen ? CloseIcon : MenuIcon}
+            width={isMenuOpen ? '14px' : '18px'}
+            className={`menu-icon ${
+              animation === 'shrinking' ? 'shrink-icon' : animation === 'growing' ? 'grow-icon' : ''
+            }`}
+            onClick={handleClick}
+            alt='menu-icon'
+          />
         </div>
-        <img src={SearchIcon} className='header-buttons-search'></img>
-        <img src={AccountIcon}></img>
-      </div>
 
+        {/* LOGO */}
+        <Link to='/'>
+          <img src={NewLogo} className='logo'></img>
+        </Link>
+
+        {/* BUTTONS */}
+        <div className='header-buttons'>
+          <div className='search-bar'>
+            <input type='text' placeholder='Search...' />
+            <img src={SearchIcon}></img>
+          </div>
+          <img src={SearchIcon} className='header-buttons-search'></img>
+          <img src={AccountIcon}></img>
+        </div>
+      </header>
       {/* MENU */}
-      <div className={`side-menu ${openSideMenu ? 'open' : ''}`}>
-        <Link to='/recipes'>Recipes</Link>
-        <Link to='/blogs'>Blogs</Link>
+      <div className='side-menu-container'>
+        <div className={`side-menu ${openSideMenu ? 'open' : ''}`}>
+          <Link to='/recipes'>
+            <span>Recipes</span>
+            <img src={ArrowIcon} height='13px' />
+          </Link>
+          <Link to='/blogs'>
+            <span>Blogs</span>
+            <img src={ArrowIcon} height='13px' />
+          </Link>
+        </div>
       </div>
-    </header>
+    </div>
   )
 }
