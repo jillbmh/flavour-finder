e69f479a8d6d40d08a65283465147be438045b3a
Jill Henderson <jillian.bethany.henderson@gmail.com>
2023-09-18 17:24:22 +0100

    create recipe component

---
+++ client/utils/auth.js
@@ -0,0 +1,35 @@
+const tokenName = 'flavourFinder-token'
+
+export function setToken(token){
+  localStorage.setItem(tokenName, token)
+}
+
+export function getToken(){
+  return localStorage.getItem(tokenName)
+}
+
+export function removeToken(){
+  return localStorage.removeItem(tokenName)
+}
+
+export function isAuthenticated(){
+  const token = getToken()
+
+  if (!token) return false
+
+
+  const payload = JSON.parse(window.atob(token.split('.')[1]))
+  const payloadExpiry = payload.exp
+  const now = Date.now() / 1000
+  const userID = payload.sub
+
+  if (payloadExpiry > now){
+    return userID
+  } else {
+
+    removeToken()
+
+    return false
+  }
+  
+}
