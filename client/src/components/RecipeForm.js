f6c08167c07d0fcc61effe1249f815bb03cd3d46
kisalier <kisalie@hotmail.com>
2023-09-18 17:25:15 +0100

    made changes to recipeform

--- client/src/components/RecipeForm.js
+++ client/src/components/RecipeForm.js
@@ -1,3 +1,18 @@
-export default function RecipeForm() {
+import { useForm } from 'react-hook-form'
+import { useState, useEffect } from 'react'
+
+
+export default function RecipeForm({ onLoad }) {
+  const { register } = useForm()
+  // State
+  const [formData, setFormData] = useState(null)
+  const [errors, setErrors] = useState('')
+
+  useEffect(() => {
+
+  }, [])
+
+
+
   return <h1>RecipeForm</h1>
 }
