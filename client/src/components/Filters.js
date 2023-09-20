f9dcba09e16a5a015bc0f3d407f8899f3b3137a3
Luke Horton <developedbyluke@gmail.com>
2023-09-20 17:13:25 +0100

    Added slider

--- client/src/components/Filters.js
+++ client/src/components/Filters.js
@@ -1,4 +1,30 @@
+import { useState, useRef } from 'react'
+
 export default function Filters({ filter, setFilter }) {
+  const [value, setValue] = useState(0)
+  const sliderRef = useRef(null)
+
+  const handleInputChange = event => {
+    const newValue = event.target.value
+    setValue(newValue)
+  }
+
+  const handleSliderChange = event => {
+    const newValue = event.target.value
+    setValue(newValue)
+  }
+
+  const sliderValuePosition = () => {
+    if (sliderRef.current) {
+      const slider = sliderRef.current
+      const trackWidth = slider.clientWidth
+      const thumbWidth = (slider.clientWidth / (slider.max - slider.min)) * (slider.step || 1)
+      const thumbPosition = (value / slider.max) * trackWidth - thumbWidth / 2
+      return { left: `${thumbPosition}px` }
+    }
+    return { left: '0' }
+  }
+
   return (
     <div className='filters-container'>
       <select value={filter.cuisine} onChange={e => setFilter(prev => ({ ...prev, cuisine: e.target.value }))}>
@@ -24,6 +50,23 @@ export default function Filters({ filter, setFilter }) {
         <option value='side'>Side</option>
         <option value='dessert'>Dessert</option>
       </select>
+
+      <div className='slider-container'>
+        <label htmlFor='spiceLevel'>Spice Level</label>
+        <input
+          id='spiceLevel'
+          ref={sliderRef}
+          type='range'
+          name='spiceLevel'
+          min='0'
+          max='5'
+          value={value}
+          onChange={handleSliderChange}
+        />
+        <span className='slider-value' style={sliderValuePosition}>
+          {value}
+        </span>
+      </div>
     </div>
   )
 }
