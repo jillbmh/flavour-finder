1a38f652dffe0941ccfead50c75ffa9b8f23ecf0
Luke Horton <developedbyluke@gmail.com>
2023-09-21 15:44:27 +0100

    Updated dropdown links and added Create Recipe to login

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -10,19 +10,17 @@ export default function Profile() {
   useEffect(() => {
     async function getRecipesByUser() {
       try {
-        // const { data } = await axios.get(`/user/${userId}`)
-        const { data } = await axios.get('/api/user/650c2cfbde13d1a3da109eff')
+        const { data } = await axios.get(`/api/user/${userId}`)
+        // const { data } = await axios.get('/api/user/650c2cfbde13d1a3da109eff')
         setUsersRecipes(data)
         console.log(userId)
+        console.log(data)
       } catch (error) {
         console.log(error.message)
       }
     }
     getRecipesByUser()
-  }, []
-
-  )
-
+  }, [])
 
   return (
     <main className='user-profile'>
@@ -31,36 +29,23 @@ export default function Profile() {
       <Link to={`/user/${userId}/create`}>
         Create New Recipe
       </Link>
+      <h2>Recipes you added:</h2>
+      <section className='grid-container'>
+        {usersRecipes.map((recipe) => (
+          <Link key={recipe._id} to={`/recipes/${recipe._id}`} className='recipe'>
+            <div
+              className='recipe-container'
+              style={{
+                backgroundImage: `url(${recipe.image})`,
+              }}
+            >
+              <div className='recipe-title'>
+                <p>{recipe.title}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </section>
     </main>
   )
 }
-
-// {usersRecipes.map((recipe) => (
-//         <Link key={recipe._id} to={`/recipes/${recipe._id}`} className='recipe'>
-//           <div
-//             className='recipe-container'
-//             style={{
-//               backgroundImage: `url(${recipe.image})`,
-//             }}
-//           >
-//             <div className='recipe-title'>
-//               <p>{recipe.title}</p>
-//       <h2>Recipes you added:</h2>
-//       <section className='grid-container'>
-//         {usersRecipes.map((recipe) => (
-//           <Link key={recipe._id} to={`/recipes/${recipe._id}`} className='recipe'>
-//             <div
-//               className='recipe-container'
-//               style={{
-//                 backgroundImage: `url(${recipe.image})`,
-//               }}
-//             >
-//               <div className='recipe-title'>
-//                 <p>{recipe.title}</p>
-//               </div>
-//             </div>
-//           </Link>
-//         ))}
-//       </section>
-//   )}
-
