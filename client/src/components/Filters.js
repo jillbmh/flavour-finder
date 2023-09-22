c0f9f426350dd39e03e3e056558b606e9b9f9512
Luke Horton <developedbyluke@gmail.com>
2023-09-22 12:17:58 +0100

    Updated recipe filter dropdown styling

--- client/src/components/Filters.js
+++ client/src/components/Filters.js
@@ -4,7 +4,7 @@ export default function Filters({ filter, setFilter }) {
   return (
     <div className='filters-container'>
       <select value={filter.cuisine} onChange={e => setFilter(prev => ({ ...prev, cuisine: e.target.value }))}>
-        <option value=''>All Cuisines</option>
+        <option value=''>Cuisine</option>
         <option value='italian'>Italian</option>
         <option value='japanese'>Japanese</option>
         <option value='chinese'>Chinese</option>
@@ -20,7 +20,7 @@ export default function Filters({ filter, setFilter }) {
       </select>
 
       <select value={filter.type} onChange={e => setFilter(prev => ({ ...prev, type: e.target.value }))}>
-        <option value=''>All Meal Types</option>
+        <option value=''>Meal Type</option>
         <option value='starter'>Starter</option>
         <option value='main'>Main</option>
         <option value='side'>Side</option>
